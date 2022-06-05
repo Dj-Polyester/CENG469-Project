659beb47c6e61b2bded0a8e534da3436d5713df0
Batuhan Karaca <45545087+Dj-Polyester@users.noreply.github.com>
2022-06-05 22:46:38 +0300

    instead of UINT_MAX just used 0 for initial value

--- include/extension.h
+++ include/extension.h
@@ -6,40 +6,37 @@ struct Extension
     // available extensions
     std::vector<VkExtensionProperties> extProps;
     // number of available extensions
-    uint32_t extCount = UINT32_MAX;
+    uint32_t extCount = 0;
     // layer vars
     std::vector<VkLayerProperties> layerProps;
-    uint32_t layerCount = UINT32_MAX;
+    uint32_t layerCount = 0;
     // glfw vars
-    uint32_t glfwExtCount = UINT32_MAX;
+    uint32_t glfwExtCount = 0;
     std::vector<const char *> glfwExtensions;
     // other
     VkResult result;
     Extension()
     {
-        if (extCount == UINT32_MAX)
-        {
-            vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
-            extProps.resize(extCount);
-            result = vkEnumerateInstanceExtensionProperties(nullptr, &extCount, extProps.data());
-            debugVkResult(result);
-        }
+        
+        vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
+        extProps.resize(extCount);
+        result = vkEnumerateInstanceExtensionProperties(nullptr, &extCount, extProps.data());
+        debugVkResult(result);
+        
         debugVkExtensions(extProps, extCount);
-        if (layerCount == UINT32_MAX)
-        {
-            vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-            layerProps.resize(layerCount);
-            result = vkEnumerateInstanceLayerProperties(&layerCount, layerProps.data());
-            debugVkResult(result);
-        }
+        
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        layerProps.resize(layerCount);
+        result = vkEnumerateInstanceLayerProperties(&layerCount, layerProps.data());
+        debugVkResult(result);
+        
         debugVkLayers(layerProps, layerCount);
-        if (glfwExtCount == UINT32_MAX)
-        {
-            const char **glfwExts = glfwGetRequiredInstanceExtensions(&glfwExtCount);
-            glfwExtensions.insert(glfwExtensions.begin(), glfwExts, glfwExts + glfwExtCount);
+        
+        const char **glfwExts = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+        glfwExtensions.insert(glfwExtensions.begin(), glfwExts, glfwExts + glfwExtCount);
 
-            glfwRequireDebugUtils();
-        }
+        glfwRequireDebugUtils();
+        
         debugGlfwExtensions(glfwExtensions, glfwExtCount);
     }
 
